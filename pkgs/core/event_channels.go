283c226f254f82ddc240d77f9a5d76ce028a8940
Besnik Picori <picori.besnik@gmail.com>
2025-05-05 13:56:48 +0200

    refactor: update EventChannels to replace appendEntries and heartbeat channels with logRequest and logResponse channels

--- pkgs/core/event_channels.go
+++ pkgs/core/event_channels.go
@@ -8,33 +8,27 @@ type Event[T any] struct {
 }
 
 type EventChannels struct {
-	voteRequestChan    chan Event[dto.VoteRequest]
-	voteResponseChan   chan Event[dto.VoteResponse]
-	appendEntriesReqCh chan Event[dto.AppendEntriesArgs]
-	appendEntriesResCh chan Event[dto.AppendEntriesReply]
-	heartbeatReqCh     chan Event[dto.AppendEntriesArgs]
-	heartbeatRespCh    chan Event[dto.AppendEntriesReply]
-	leaderElectedCh    chan Event[bool]
+	voteRequestChan  chan Event[dto.VoteRequest]
+	voteResponseChan chan Event[dto.VoteResponse]
+	logRequestChan   chan Event[dto.LogRequest]
+	logResponseChan  chan Event[dto.LogResponse]
+	leaderElectedCh  chan Event[bool]
 }
 
 func NewEventLoop() *EventChannels {
 	return &EventChannels{
-		voteRequestChan:    make(chan Event[dto.VoteRequest]),
-		voteResponseChan:   make(chan Event[dto.VoteResponse]),
-		appendEntriesReqCh: make(chan Event[dto.AppendEntriesArgs]),
-		appendEntriesResCh: make(chan Event[dto.AppendEntriesReply]),
-		heartbeatReqCh:     make(chan Event[dto.AppendEntriesArgs]),
-		heartbeatRespCh:    make(chan Event[dto.AppendEntriesReply]),
-		leaderElectedCh:    make(chan Event[bool]),
+		voteRequestChan:  make(chan Event[dto.VoteRequest]),
+		voteResponseChan: make(chan Event[dto.VoteResponse]),
+		logRequestChan:   make(chan Event[dto.LogRequest]),
+		logResponseChan:  make(chan Event[dto.LogResponse]),
+		leaderElectedCh:  make(chan Event[bool]),
 	}
 }
 
 func (el *EventChannels) Close() {
 	close(el.voteRequestChan)
 	close(el.voteResponseChan)
-	close(el.appendEntriesReqCh)
-	close(el.appendEntriesResCh)
-	close(el.heartbeatReqCh)
-	close(el.heartbeatRespCh)
+	close(el.logRequestChan)
+	close(el.logResponseChan)
 	close(el.leaderElectedCh)
 }
