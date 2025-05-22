413ddeafba011525f2a09722adefd86ac7dded2e
Besnik Picori <picori.besnik@gmail.com>
2025-05-23 00:23:52 +0200

    feat: add lpush command in client

--- cmd/client/commands/util.go
+++ cmd/client/commands/util.go
@@ -12,21 +12,29 @@ func ClearScreen() {
 
 func PrintHelp() {
 	fmt.Println("\nAvailable commands:")
-	fmt.Println("  status            - Get the cluster status")
-	fmt.Println("  set <key> <value> - Add a key to the storage")
-	fmt.Println("  rm <key>          - Remove a key from the storage")
-	fmt.Println("  clear/cls         - Clear the screen")
-	fmt.Println("  help              - Show this help message")
-	fmt.Println("  exit/quit         - Exit the CLI")
+	fmt.Println("  status                                      - Get the cluster status")
+	fmt.Println("  set <key> <value>                           - Add a key to the storage")
+	fmt.Println("  get <key>                                   - Get a value from the storage")
+	fmt.Println("  incr <key>                                  - Increment a numeric value")
+	fmt.Println("  decr <key>                                  - Decrement a numeric value")
+	fmt.Println("  rm <key>                                    - Remove a key from the storage")
+	fmt.Println("  lpush <key> <element> [element2] [element3] - Prepend elements to list")
+	fmt.Println("  clear/cls                                   - Clear the screen")
+	fmt.Println("  help                                        - Show this help message")
+	fmt.Println("  exit/quit                                   - Exit the CLI")
 	fmt.Println()
 }
 
 func ShowUsage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s -servers=host1:port1,host2:port2,... [operation] [arguments...]\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "Operations:\n")
-	fmt.Fprintf(os.Stderr, "  status            - Get the cluster status\n")
-	fmt.Fprintf(os.Stderr, "  set <key> <value> - Add a key to the storage\n")
-	fmt.Fprintf(os.Stderr, "  rm <key>          - Remove a key from the storage\n")
+	fmt.Fprintf(os.Stderr, "  status                                      - Get the cluster status\n")
+	fmt.Fprintf(os.Stderr, "  set <key> <value>                           - Add a key to the storage\n")
+	fmt.Fprintf(os.Stderr, "  get <key>                                   - Get a value from the storage\n")
+	fmt.Fprintf(os.Stderr, "  incr <key>                                  - Increment a numeric value\n")
+	fmt.Fprintf(os.Stderr, "  decr <key>                                  - Decrement a numeric value\n")
+	fmt.Fprintf(os.Stderr, "  rm <key>                                    - Remove a key from the storage\n")
+	fmt.Fprintf(os.Stderr, "  lpush <key> <element> [element2] [element3] - Prepend elements to list\n")
 	fmt.Fprintf(os.Stderr, "If no operation is provided, the CLI will start in interactive mode.\n")
 	flag.PrintDefaults()
 }
